3e7bd6d80084a0c826b1f1d1f91d6048706b06f0
Andrei <andreibu.ancora@gmail.com>
2019-11-01 21:03:43 +0200

    mesaj

---
+++ src/main/java/TestMerge.java
@@ -0,0 +1,13 @@
+public class TestMerge {
+
+    private String anaAreMere;
+
+
+    public String getAnaAreMere() {
+        return anaAreMere;
+    }
+
+    public void setAnaAreMere(String anaAreMere) {
+        this.anaAreMere = anaAreMere;
+    }
+}
