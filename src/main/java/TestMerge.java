6a48fb08b35e2efaffe9e4befb5ee530b1d12242
Gergely Laszlo Arpad <gergely.laszlo.a@gmail.com>
2019-11-02 15:57:22 +0200

    Y

--- src/main/java/TestMerge.java
+++ src/main/java/TestMerge.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class TestMerge {
+    public int nume;
+
+    public int getNume() {
+        System.out.println("x");
+        return nume;
+    }
+
+    public void setNume(int nume) {
+        this.nume = nume;
+    }
 }
